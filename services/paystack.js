97d2c5b74dabb93e978f137711bdabe4110d0fc1
nexpaksa-beep <nexpaksa@gmail.com>
2026-07-04 10:19:04 +0200

    Update paystack.js

--- services/paystack.js
+++ services/paystack.js
@@ -3,7 +3,6 @@ const axios = require("axios");
 const PAYSTACK_SECRET = process.env.PAYSTACK_SECRET_KEY;
 
 async function initializePayment(email, amount, reference) {
-
     const response = await axios.post(
         "https://api.paystack.co/transaction/initialize",
         {
@@ -22,6 +21,20 @@ async function initializePayment(email, amount, reference) {
     return response.data;
 }
 
+async function verifyPayment(reference) {
+    const response = await axios.get(
+        `https://api.paystack.co/transaction/verify/${reference}`,
+        {
+            headers: {
+                Authorization: `Bearer ${PAYSTACK_SECRET}`
+            }
+        }
+    );
+
+    return response.data;
+}
+
 module.exports = {
-    initializePayment
+    initializePayment,
+    verifyPayment
 };
